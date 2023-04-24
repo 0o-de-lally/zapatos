77338770294b4f2249b1b7a16c90393b1395bb76
Gokhan Simsek <g.simsek@paralect.com>
2023-04-24 11:28:05 +0300

    Minor changes/updates
    Minor changes/updates
    
    Changes for first working v7 smoke-tests
    
    Adding ol_vdf native module and test
    
    Additional change for previous commit
    
    Accept MRB_PATH at runtime
    
    Disabling 0L native fns due to build error: cyclic package dependency: package getrandom
    
    Accept ZAPATOS_BIN_PATH at runtime

--- crates/aptos/src/node/mod.rs
+++ crates/aptos/src/node/mod.rs
@@ -1153,7 +1153,7 @@ impl CliCommand<()> for RunLocalTestnet {
                 Some(test_dir_copy),
                 false,
                 false,
-                aptos_cached_packages::head_release_bundle(),
+                &aptos_cached_packages::head_release_bundle(),
                 rng,
             );
             eprintln!("Node stopped unexpectedly {:#?}", result);
